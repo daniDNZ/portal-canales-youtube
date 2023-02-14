0fd4af577d086848cea58eeac2283832c41550e8
danisanz <daniDNZ@users.noreply.github.com>
2023-02-14 23:30:16 +0100

    Add Channel Tabs

--- src/components/ChannelTabs.tsx
+++ src/components/ChannelTabs.tsx
@@ -1,6 +1,6 @@
 import { Box, Tab, Tabs } from "@mui/material";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useMemo, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function a11yProps(index: number) {
   return {
@@ -11,16 +11,20 @@ function a11yProps(index: number) {
 
 export default function ChannelTabs() {
   const navigate = useNavigate();
-  const [tab, setTab] = useState(0);
+  const location = useLocation();
+  const routes = ["/", "/dashboard"];
+  // Active the correct tab
+  const [tab, setTab] = useState(
+    routes.findIndex((path) => path === location.pathname)
+  );
 
   const handleChange = (_ev: React.SyntheticEvent, newTab: number) => {
     setTab(newTab);
-    const routes = ["videos", "dashboard"];
     navigate(routes[newTab]);
   };
   return (
     <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-      <Tabs value={tab} onChange={handleChange} aria-label="basic tabs example">
+      <Tabs value={tab} onChange={handleChange} aria-label="Basic tabs">
         <Tab label="Videos" {...a11yProps(0)} />
         <Tab label="Statistics" {...a11yProps(1)} />
       </Tabs>
