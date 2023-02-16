40820bab104e6caff15b75f8baa66996772fea37
danisanz <daniDNZ@users.noreply.github.com>
2023-02-16 21:58:13 +0100

    Add config

---
+++ src/config.sample.ts
@@ -0,0 +1,10 @@
+export const LOCAL_STORAGE_TAG = "ytportal_usr"
+
+export const YT_API_URL = "https://www.googleapis.com/youtube/v3/"
+
+// Change this for your API key
+export const YT_API_KEY = "<API_KEY>"
+
+// true = Carga datos mock
+// false = Pide los datos a la API
+export const LOAD_MOCK_DATA = false
